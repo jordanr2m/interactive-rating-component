a470de6747581b683fdb2132b54308b93fa78af5
Jordan J <jordanr2m@gmail.com>
2023-10-11 13:49:06 -0700

    Finished JS.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
-// Grab rating buttons. No space between button and class name
-const ratingButtons = document.querySelectorAll("button.circle");
-console.log(ratingButtons);
+// Submit button
+const submitButton = document.querySelector(".submit");
+// Starting screen
+const startWindow = document.querySelector(".start-window");
+// Ending screen
+const thankYouWindow = document.querySelector(".thank-you-message");
+// Rating span
+const ratingSpan = document.querySelector(".rating-selection span");
+// console.log(ratingSpan);
 
-const submitRating = function () {
-    
-}
+// Grab all number buttons. No space between words
+let buttonList = document.querySelectorAll("button.circle");
+
+let userRating = "";
+
+// Make a # rating for each user selection
+buttonList.forEach(function (selection) {
+    selection.addEventListener("click", function (e) {
+        userRating = e.target.innerHTML;
+        // console.log(userRating);
+    })
+})
+
+// Submit button functionality
+submitButton.addEventListener("click", function (e){
+    startWindow.classList.toggle("hidden");
+    thankYouWindow.classList.toggle("hidden");
+    ratingSpan.innerHTML = userRating;
+})
