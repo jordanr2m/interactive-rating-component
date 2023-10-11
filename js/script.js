b0e5f646054269eb9ecc00414823f669c8e13868
Jordan J <jordanr2m@gmail.com>
2023-10-11 12:54:50 -0700

    Completed HTML and CSS.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Grab rating buttons. No space between button and class name
+const ratingButtons = document.querySelectorAll("button.circle");
+console.log(ratingButtons);
+
+const submitRating = function () {
+    
+}
